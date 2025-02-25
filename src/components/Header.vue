b84c773e493196cc4a929d48b6fdcf808dc00031
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2025-02-25 16:37:34 +0100

    Header quasi finito

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+	data() {
+		return {
+			//   title: "Il Mio Titolo",
+		};
+	},
+};
+</script>
+
+<template>
+	<!-- <h1>{{ title }}</h1> -->
+	<div class="w-full h-[350px] overflow-hidden">
+		<div
+			class="flex justify-between items-center bg-[#343434] text-[#f7fff7] p-4"
+		>
+			<a href="#" class="text-[32px]">Ez+ LoL</a>
+			<div class="grid grid-cols-2 gap-4 text-[20px]">
+				<a href="#">Home</a>
+				<a href="#">Su di me</a>
+			</div>
+		</div>
+		<img src="../../public/Banner-Qq7bNmtVH-transformed.jpeg" alt="" />
+	</div>
+</template>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,15 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 
 defineProps({
-  msg: String,
-})
+	msg: String,
+});
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
-  <!-- <h1>{{ msg }}</h1>
+	<!-- <h1>{{ msg }}</h1>
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
@@ -36,13 +36,11 @@ const count = ref(0)
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
   <p class="text-3xl font-bold underline">prova</p> -->
 
-  <div>
-    
-  </div>
+	<div></div>
 </template>
 
 <style scoped>
 .read-the-docs {
-  color: #888;
+	color: #888;
 }
 </style>
